7699b7691cde34c4fcee4d1fafc53e62bcfa30cb
rmanzanet <ridder.manzanet@gmail.com>
2025-03-17 22:11:31 -0700

    Update script.js
    Added beginning code.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let id          = document.getElementById('id');                    // THE EMPLOYEE ID TEXT BOX
+let name        = document.getElementById('name');                  // THE FULL NAME TEXT BOX
+let ext         = document.getElementById('ext');                   // THE EXTENSION TEXT BOX
+let email       = document.getElementById('email');                 // THE EMAIL TEXT BOX
+let department  = document.getElementById('department');            // THE DEPARTMENT TEXT BOX
+let submit      = document.getElementById('submit');                // THE ADD EMPLOYEE BUTTON
+
